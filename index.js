302489f6591ee0142290df7a018ccc902236f84b
overlife3 <vip.kostya.semenov.2004@mail.ru>
2023-02-25 18:08:28 +0300

    i

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ import express from "express"
 const app = express()
 const port = 5000
 
-app.use('/', (req, res) => {
+app.use('/api', (req, res) => {
 	res.json({message: "Hello from express"})
 })
 
-app.get('/api', (req, res) => {
-	res.send('hello')
-})
+// app.get('/api', (req, res) => {
+// 	res.send('hello')
+// })
 
 app.listen(process.port || port, () => {
 	console.log(`starting server on port: ${port}`)
