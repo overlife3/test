66a5b968e56512f527c3f5bf67625a1483f13bf1
overlife3 <vip.kostya.semenov.2004@mail.ru>
2023-02-25 17:25:26 +0300

    create server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log(1)
+import express from "express"
+
+const app = express()
+const port = 5000
+
+app.use('/', (req, res) => {
+	res.json({message: "Hello from express"})
+})
+
+app.listen(9000, () => {
+	console.log(`starting server on port: ${port}`)
+})
